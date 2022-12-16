0c9aefe56ff50709e126523703f851ef5d1352ac
Amanda Costa Pereira <105894938+Amanda-653@users.noreply.github.com>
2022-12-16 17:03:44 -0300

    Add files via upload

---
+++ a.php
@@ -0,0 +1,22 @@
+<?php
+
+$arr = explode(" ",$posttitle);
+$url=implode("-",$arr);
+$imgfile = $_FILES["postimage"]["name"];
+// get the image extension
+$extension = substr($imgfile,strlen($imgfile)-4,strlen($imgfile));
+// allowed extensions
+$allowed_extensions = array(".jpg","jpeg",".png",".gif");
+// Validation for allowed extensions .in_array() function searches an array for a specific value.
+if(!in_array($extension,$allowed_extensions))
+{
+echo "<script>alert('Invalid format. Only jpg / jpeg/ png /gif format allowed');</script>";
+}
+else
+{
+//rename the image file
+$imgnewfile=md5($imgfile).$extension;
+// Code for move image into directory
+move_uploaded_file($_FILES["postimage"]["tmp_name"],"postimages/".$imgnewfile);
+}
+?>
---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+mysqli_query($con, "SET NAMES 'utf8'");
+mysqli_query($con, 'SET character_set_connection=utf8');
+mysqli_query($con, 'SET character_set_client=utf8');
+mysqli_query($con, 'SET character_set_results=utf8');
+?>
